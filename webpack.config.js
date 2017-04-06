d337fce892c0a8a846feadae361a2c2bd55f555f
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-04-06 12:25:30 -0700

    THREE hello world scene running

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var DEV = path.resolve(__dirname, "dev");
+var DIST = path.resolve(__dirname, "dist");
+
+module.exports = {
+  entry: DEV+ '/app.js',
+  output: {
+    path: DIST,
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        include: DEV,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+};
